c10d1b2abd36232ad6017efdb4e1656998348f27
juanchistosomk <juankarlitos22@hotmail.com>
2021-05-18 22:31:02 -0500

    ajuste query dql

--- dql/7.selectOnMENDataBase.sql
+++ dql/7.selectOnMENDataBase.sql
@@ -1,10 +1,10 @@
 SET search_path = situacion_academica_2018_2019;
 
+/* SELECT * FROM  men_sa_2018_2019; */
 
-SELECT * FROM  men_sa_2018_2019;
-
-SELECT sede_codigo FROM men_sa_2018_2019 ORDER BY sede_codigo DESC;
+SELECT * FROM situacion_academica_2018_2019.men_sa_2018_2019;
+SELECT sede_codigo FROM situacion_academica_2018_2019.men_sa_2018_2019 ORDER BY sede_codigo DESC;
 
 SELECT situacade_nombre, count(jornsitu_cantidad_hombre) as TotalHombres, count(jornsitu_cantidad_mujer) as TotalMujeres
-FROM men_sa_2018_2019
+FROM situacion_academica_2018_2019.men_sa_2018_2019
 GROUP BY situacade_codigo;
